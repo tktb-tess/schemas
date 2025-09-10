c2fdd13a7f526d1060c1477139e924e164209f04
Born-Bohrer <chem1000leaves@gmail.com>
2025-09-11 01:54:43 +0900

    prepare to publish

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,14 +1,35 @@
 import { OTMJSON, parseAndValidate } from '@tktb-tess/my-zod-schema';
+import { unified } from 'unified';
+import RemarkParse from 'remark-parse';
+import RemarkGfm from 'remark-gfm';
+import RemarkRehype from 'remark-rehype';
+import RehypeSanitize from 'rehype-sanitize';
+import RehypeStringify from 'rehype-stringify';
+
+const mdToHtml = async (md: string) => {
+  return unified()
+    .use(RemarkParse)
+    .use(RemarkGfm)
+    .use(RemarkRehype)
+    .use(RehypeSanitize)
+    .use(RehypeStringify)
+    .process(md)
+    .then((v) => v.toString());
+};
 
 const main = async () => {
   const resp = await fetch('/sample-otm.json').then((r) => r.text());
-  const result = parseAndValidate(resp, OTMJSON.zpdicOtmjsonSchema);
+  const result = parseAndValidate(resp, OTMJSON.zpdicOtmSchema);
   if (result.success) {
     const { data } = result;
     data.words.sort(({ entry: a }, { entry: b }) => a.id - b.id);
     data.examples.sort(({ id: a }, { id: b }) => a - b);
-    console.log(...data.words);
-    console.log(...data.examples);
+    
+    const expr = await mdToHtml(data.zpdicOnline.explanation ?? '');
+
+    const app = document.getElementById('app');
+    if (!app) return;
+    app.innerHTML = expr;
   }
 };
 
