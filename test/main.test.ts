c51df424257a1d9fd97df12517e969832d1e10d8
Born-Bohrer <chem1000leaves@gmail.com>
2025-09-12 17:56:34 +0900

    test

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,8 +3,8 @@ import { OTMJSON, parseAndValidate } from '@tktb-tess/my-zod-schema';
 import { readFileSync } from 'node:fs';
 
 it('test1', () => {
-  const str = readFileSync('./public/sample-otm.json', { encoding: 'utf8' });
-  const result = parseAndValidate(str, OTMJSON.zpdicOtmjsonSchema);
+  const str = readFileSync('sample-otm.json', { encoding: 'utf8' });
+  const result = parseAndValidate(str, OTMJSON.zpdicOtmSchema);
 
   if (!result.success) {
     expect.unreachable('failed to parse!');
@@ -16,8 +16,10 @@ it('test1', () => {
 
   const ids: readonly number[] = words.map(({ entry }) => entry.id);
 
-  ids.forEach((id, i, ids) => {
+  const res = ids.map((id, i, ids) => {
     const prev = ids[i - 1] ?? 0;
-    console.log(id - prev === 0);
-  });
+    return id - prev === 0;
+  }).every((v) => !v);
+
+  expect(res).toBe(true);
 });
